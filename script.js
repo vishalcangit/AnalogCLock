2fc61e5d9661b2f6bc9b86f5b12128edd9e92239
vishalcangit <vishalsuvarna000@gmail.com>
2022-08-09 18:27:53 +0530

    js done

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const secHand = document.querySelector('.seconds-hand');
+const minHand = document.querySelector('.minutes-hand');
+const hrHand = document.querySelector('.hour-hand');
 
 const setDate = () => {
     const now = new Date();
+
     const seconds = now.getSeconds();
     const secDeg = ((seconds / 60) * 360)+90;
     secHand.style.transform=`rotate(${secDeg}deg)`
+
+    const minutes = now.getMinutes();
+    const minDeg = ((minutes / 60) * 360)+90;
+    minHand.style.transform=`rotate(${minDeg}deg)`
+
+    const hr = now.getHours();
+    const hrDeg = ((hr / 60) * 360)+90;
+    hrHand.style.transform=`rotate(${hrDeg}deg)`
+
 }
 
 setInterval(setDate, 1000)
